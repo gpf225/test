567ae1dd2e14b074fcf6795e01949d3add244838
20200089 <790907756@qq.com>
2021-12-11 21:49:05 +0800

    修改

--- define.cpp
+++ define.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
         
 using namespace std;
-        
-//#define  OUTPUT(A) cout<<A<<":"<<(A)<<endl;
-#define  OUTPUT(A) cout<<#A<<":"<<(A)<<endl;
 
-#define Func_A \
-    do{\
-        cout << "Func__a" << endl;\
-    }while(0)
-#define Func(A) Func_##A  // 两个是拼贴，一个是字符串化“” 
+
+// https://zhuanlan.zhihu.com/p/344240420 宏定义展开规则
+// GO_LABEL(GO_LABEL_HEAD,__LINE__) GO_LABEL宏中没有"# ##"则进入下一层，下一层中
+// 展开GO_LABEL_HEAD 和 __LINE__， 然后回到上一层执行GO_LABEL2(GoLabel_,21)
+
+#define GO_LABEL_HEAD GoLabel_
+#define GO_LABEL2(s1, s2) s1##s2
+#define GO_LABEL(s1, s2) GO_LABEL2(s1, s2)
+
+#define STRINGIFY(S) #S
+#define DEFER_STRINGIFY(S) STRINGIFY(S)
+#define GO_LABEL_STR_ DEFER_STRINGIFY(GO_LABEL_HEAD) "_" DEFER_STRINGIFY(__LINE__)
+#define GO_LABEL_STR DEFER_STRINGIFY(GO_LABEL_HEAD) "" DEFER_STRINGIFY(__LINE__)
+
+#define EXAMPLE \
+    do{ \
+        void* pLable = NULL; \
+        pLable = &&GO_LABEL(GO_LABEL_HEAD,__LINE__); \
+        GO_LABEL(GO_LABEL_HEAD,__LINE__): \
+        cout << (char*)GO_LABEL_STR_ << endl; \
+        if(pLable) \
+        { \
+            pLable = NULL; \
+            goto *pLable; \
+        }\
+        cout << (char*)GO_LABEL_STR << endl; \
+    } while (0)
+
 
 int main()
 {
-    int a=1,b=2;
-        
-    OUTPUT(a);
-    OUTPUT(b);
-    OUTPUT(a+b);
-    static int n = 3;
-    Func(A) ;
-    void* pLabl = NULL;
-    pLabl = &&LABEL1; 
-    LABEL1:
-        cout << "label1"<< endl;
-    LABEL3:
-        cout << "label3"<< endl;
-    --n;
-    if(n<=0)
-        goto LABEL2; 
-goto *pLabl;
-
-    LABEL2:
-       cout << "label2" << endl; 
-    return 1;
+    EXAMPLE;
 }
